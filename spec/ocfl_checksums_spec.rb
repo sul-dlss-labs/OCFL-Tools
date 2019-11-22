1155b160d22caf7c55b01712f2b5f13262523b06
Julian M. Morley <jmorley@stanford.edu>
2019-11-21 22:40:49 -0800

    Fix local_path for checksums rspec

--- spec/ocfl_checksums_spec.rb
+++ spec/ocfl_checksums_spec.rb
@@ -10,7 +10,9 @@ describe OcflTools::OcflValidator do
   OcflTools.config.content_directory = 'data'
 
   # TODO: fix expand_path so we're not tying the results to my local machine's directory structure.
-  local_path = object_a.delete_suffix('/spec/fixtures/validation/object_a')
+  # local_path = object_a.delete_suffix('/spec/fixtures/validation/object_a')
+  # local_path = File.join(File.dirname(__dir__), 'spec', 'fixtures', 'validation', 'object_a')
+
   # local_path should stay the same for all fixtures.
 
   describe 'perfect object a' do
@@ -23,33 +25,35 @@ describe OcflTools::OcflValidator do
 
   object_e = File.expand_path('./spec/fixtures/validation/object_e')
   validate_e = OcflTools::OcflValidator.new(object_e)
+  local_path_e = File.join(File.dirname(__dir__), 'spec', 'fixtures', 'validation', 'object_e')
 
   describe 'object e is missing a file on disk' do
     it 'checks checksums from manifest' do
       expect(validate_e.verify_checksums.all).to match(
-        'error' => { 'E111' => { 'verify_checksums' => ["#{local_path}/spec/fixtures/validation/object_e/v0003/data/my_content/dickens.txt in inventory but not found on disk."] } }, 'warn' => {}, 'info' => {}, 'ok' => {}
+        'error' => { 'E111' => { 'verify_checksums' => ["#{local_path_e}/v0003/data/my_content/dickens.txt in inventory but not found on disk."] } }, 'warn' => {}, 'info' => {}, 'ok' => {}
       )
     end
   end
 
   object_f = File.expand_path('./spec/fixtures/validation/object_f')
   validate_f = OcflTools::OcflValidator.new(object_f)
-
+  local_path_f = File.join(File.dirname(__dir__), 'spec', 'fixtures', 'validation', 'object_f')
   describe 'object f has a file on disk version 3 that does not exist in manifest version 3' do
     it 'checks checksums from manifest' do
       expect(validate_f.verify_checksums.all).to match(
-        'error' => { 'E111' => { 'verify_checksums' => ["#{local_path}/spec/fixtures/validation/object_f/v0003/data/my_content/dickens.txt found on disk but missing from inventory.json."] } }, 'warn' => {}, 'info' => {}, 'ok' => {}
+        'error' => { 'E111' => { 'verify_checksums' => ["#{local_path_f}/v0003/data/my_content/dickens.txt found on disk but missing from inventory.json."] } }, 'warn' => {}, 'info' => {}, 'ok' => {}
       )
     end
   end
 
   object_g = File.expand_path('./spec/fixtures/validation/object_g')
   validate_g = OcflTools::OcflValidator.new(object_g)
+  local_path_g = File.join(File.dirname(__dir__), 'spec', 'fixtures', 'validation', 'object_g')
 
   describe 'object g has a bad digest in the manifest file' do
     it 'checks checksums from manifest' do
       expect(validate_g.verify_checksums.all).to match(
-        'error' => { 'E111' => { 'verify_checksums' => ["#{local_path}/spec/fixtures/validation/object_g/v0003/data/my_content/dickens.txt digest in inventory does not match digest computed from disk"] } }, 'warn' => {}, 'info' => {}, 'ok' => {}
+        'error' => { 'E111' => { 'verify_checksums' => ["#{local_path_g}/v0003/data/my_content/dickens.txt digest in inventory does not match digest computed from disk"] } }, 'warn' => {}, 'info' => {}, 'ok' => {}
       )
     end
   end
