fea4ae10f42063ccb8a7ae6ecca8b29d0a382d79
Julian M. Morley <jmorley@stanford.edu>
2020-02-28 08:46:13 -0800

    Prep for using canonical fixture info in rspec

---
+++ test-it.sh
@@ -0,0 +1,11 @@
+rspec spec/ocfl_object_spec.rb
+rspec spec/ocfl_inventory_spec.rb
+rspec spec/ocfl_checksums_spec.rb
+# rspec spec/ocfl_deposit_spec.rb
+rspec spec/ocfl_delta_spec.rb
+rspec spec/ocfl_results_spec.rb
+rspec spec/ocfl_verify_spec.rb
+rspec spec/ocfl_validator_spec.rb
+
+
+# rspec spec/ocfl_utils_file_spec.rb
